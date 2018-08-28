dc8c2482a6525a8071359ab76585d4b66cbd06e2
John M. Long <36753682+John-Michael-Long@users.noreply.github.com>
2018-08-28 15:48:18 -0500

    install basic dependencies, configure webpack, create basic server and react component

---
+++ client/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import style from './styles/main.css';
+import App from './components/App.jsx';
+//import other components
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
+//WHEN USING REDUX UNCOMMENT BELOW:
+
+// import store from './store/store.js';
+// import { render } from 'react-dom';
+// import { Provider } from 'react-redux';
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube} />
+//   </Provider>,
+//   document.getElementById('app')
+// );
