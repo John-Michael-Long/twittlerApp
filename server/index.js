2787700386698ae5522b8acd62ccab304fad8735
John M. Long <36753682+John-Michael-Long@users.noreply.github.com>
2019-02-21 00:16:17 -0600

    created server and implemented socket.io

--- server/index.js
+++ server/index.js
@@ -1,15 +1,63 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('../database');
-
 const app = express();
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
+const io = require('socket.io').listen(server);
+
+
+const bodyParser = require('body-parser');
+const path = require('path');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static(__dirname + '/../public'));
+// app.use(express.static(__dirname + '/../public')); 
 
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/../client/src/index.html'));
+})
+
+app.get('/app.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/../client/src/app.js'));
+})
+
+io.on('connection', function(socket){
+  console.log('a user connected!')
+
+  socket.on('new-client-msg', function(msg){
+    console.log('message: ', msg)
+
+    io.emit('new-server-msg', msg);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
+
+
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   );
+//   if (req.method === 'OPTIONS') {
+//     res.header(
+//       'Access-Control-Allow-Mehods',
+//       'GET, POST, PUT, PATCH, DELETE'
+//     );
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
+
+
+
+
